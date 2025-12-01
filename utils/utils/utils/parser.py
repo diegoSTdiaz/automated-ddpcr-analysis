5a9e73aa15bd6a690feee4bf65bfd8451523ba0f
diegoSTdiaz <diego.diaz@sonothera.com>
2025-11-30 21:41:45 -0800

    Delete utils/utils/utils directory

--- utils/utils/utils/parser.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def parse_qxmanager_csv(df):
-    df = df.copy()
-    df["Well"] = df["Well"].astype(str).str.upper()
-    df["Copies/µL"] = pd.to_numeric(df["Copies/µL"], errors="coerce").fillna(0)
-    return df
